1c488feb271c46b86f437fd43961927b3ba05e30
Henning Horn <hh@henninghorn.dk>
2012-04-09 20:13:41 +0200

    Init

---
+++ index.php
@@ -0,0 +1,260 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf8">
+	<meta name="description" content="Henning Horn, innovativ webudvikler med passion for ui- og ux-design, med kompetencer inden for PHP5, HTML5, CSS3, jQUery." /> 
+	<meta name="keywords" content="Henning Horn freelance photography webdevelopment webdesign henninghorn hhdk danish denmark webudvikling webudvikler hjemmeside hjemmesider udvikling php linux ajax web web2.0 aktiespillet09 aktiespil aktiespillet aktiespillet.com" />
+	<title>Henning Horn</title>
+	<link rel="stylesheet" href="inc/stylesheet.css">
+	<script type="text/javascript">
+		// rM = revealMail
+		function rM() {
+			// d = domain
+			// m = mail address
+			var d = 'henninghorn.' + 'dk';
+			var m = 'hh' + '@' + d;
+
+			// Let's write to the document
+			document.getElementById('epost').innerHTML = '<a href="mailto:' + m + '">' + m + "</a>";
+		}
+	</script>
+</head>
+<body onload="rM()">
+
+<div id="sidebar">
+	<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/572453_652388354_758654117_n.jpg" alt="Henning Horn">
+</div>
+
+<div id="page">
+	<h1>Henning Horn</h1>
+	<p id="intro">
+		Innovativ webudvikler med passion for ui- &amp; ux-design
+	</p>
+
+	<h2>Kompetencer</h2>
+	<ul>
+		<li>PHP, jQuery, HTML5 + CSS3</li>
+		<li>Linux (Debian-baseret), LAMP opsætning m.m.</li>
+		<li>Git versionsstyring</li>
+	</ul>
+	<p>
+		Jeg er dybt engageret i at lære om nye teknologier o.lign.<br/>
+		<strong>Fokus pt</strong>: VPS-optimering og real-time web-applikationer.
+	</p>
+	<p>
+		Jeg udvikler i <a href="http://www.sublimetext.com/2">Sublime Text 2</a>. Bruger <a href="http://git-scm.com/">Git</a> til versionsstyring.<br/>
+		Alt mit arbejde finder sted i <a href="http://www.apple.com/dk/macosx/">OS X</a> på min <a href="http://www.apple.com/dk/macbookpro/">Macbook Pro</a>.<br/>
+	</p>
+	<p>
+		Mit foretrukne PHP-framework er <a href="http://laravel.com/">Laravel</a>. 
+		Det er et utroligt veldokumenteret og overskueligt framework, med et fantastisk community, 
+		som jeg bestemt vil anbefale.
+	</p>
+
+	<p>
+		Udover programmering interesserer jeg mig også for fotografering og billedredigering.
+	</p>
+
+	<h2>Udvalgte referencer</h2>
+	<ul>
+		<li>
+			<p>
+				<strong>2009: Aktiespillet - <a href="http://aktiespillet.com/">vis side</a></strong><br/>
+				Aktiespil med 10.000+ registrede brugere.<br/>
+				Udviklet på egen hånd i 2008/09.<br/>
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2012: Aktiespillet "2" - under udvikling</strong><br/>
+				Efterfølgeren til det nuværende aktiespil.<br/>
+				Denne version bygger på <a href="http://laravel.com">Laravel</a> og har en masse nye features.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2012: Drupal-installation-administrations-backend - under udvikling</strong><br/>
+				Freelance opgave for <a href="http://pixelmade.dk">Pixelmade</a> i 2012.<br/>
+				Systemet bruges til at oprette Drupal installationer under subdomæner.
+			</p>
+		</li>
+	</ul>
+
+	<h2>Uddannelse</h2>
+
+	<img src="img/hhx.jpg" alt="HHX" class="photo">
+
+	<ul>
+		<li>2007 - 2010: Økonomi A + IT A, HHX, Svendborg Erhvervsskole</li>
+	</ul>
+	<p>
+		Til august/september starter jeg på IT-studiet på Aarhus Universitet
+	</p>
+
+	<h2>Job &amp; erfaring</h2>
+	<ul>
+		<li>
+			<p>
+				<strong>I dag: Freelance</strong><br/>
+				Jeg driver egen virksomhed hvor jeg laver udviklingsopgaver på freelance-basis. 
+				Derudover udforsker jeg også branchen i min fritid.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2010 - 2012: Klubmedarbejder - Svendborg Erhvervsskole</strong><br/>
+				Klubmedarbejder i Skårup Ungdomsklub, en klub for unge mellem 13 og 18 år. <br/>
+				Drev en ungdomsredaktion en enkelt sæson og var i en kort periode klubleder.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2010 - 2012: IT-medarbejder - Svendborg Ungdomsskole</strong><br/>
+				Vedligholdelse af klub-computere og skolens hjemmeside.<br/>
+				Udvikling af webbaserede bookingsystemer.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2010 - 2011: Underviser - Svendborg Ungdomsskole</strong><br/>
+				Underviste i grundlæggende HTML og PHP.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2009 - 2011: Fotojournalist og redaktør - Fyns Amts Avis</strong><br/>
+				Frivilligt arbejde på Fyns Amts Avis' ungdomsredaktion.<br/>
+				I starten som fotojournalist, men til slut også som redaktør.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2009 - 2010: IT-chef v/ "Vice TV", Svendborg Erhvervsskole</strong><br/>
+				Ansvar og koordinering af udgivelsesplatformen til elev TV-stationen Vice TV.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2007 - 2007: Udvikler - Uptime-IT</strong><br/>
+				Fuldtidsansat udvikler i perioden marts/december.<br/>
+				Videreudvikling af eksisterende systemer, samt grundlægning af nye.<br/>
+				Sprog og teknologier: PHP, Linux, MySQL, Javascript, HTML.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2006 - 2007: Opvasker - Hotel Ærø</strong><br/>
+				Opvasker i knap 8 måneder.
+			</p>			
+		</li>
+		<li>
+			<p>
+				<strong>2005 - 2005: PHP-udvikler - TV2 | Interaktiv</strong><br/>
+				Én uges erhvervspraktik hos TV2s afdeling i Odense, hvor jeg blev introduceret for objektorienteret programmering.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>2003 - 2004: Avisbud - Fyns Amts Avis</strong><br/>
+				Afløser i lokalområdet.
+			</p>
+		</li>
+	</ul>
+
+	<h2>Rejser &amp; oplevelser</h2>
+	<p>
+		At rejse er at leve.
+	</p>
+
+	<img src="img/thailand.jpg" alt="" class="photo">
+
+	<ul>
+		<li>
+			<p>
+				<strong>December 2011: Nord- og sydthailand</strong><br/>
+				1 måneds familierejse sammen med vores tidligere thailandske udvekslingsstudent. 
+				Bungee-jump, jetski og elefantridning og en masse andre ting.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>November 2011: Belfast, Nordirland</strong><br/>
+				5-dages europæisk "Appetizer"-kursus i forbindelse med Svendborg Ungdomsskoles internationalisering.<br/>
+				Et introduktions kursus i mulighederne for EU-støtte til elev-udveksling og læringsformer.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>August 2011: Side, Tyrkiet</strong><br/>
+				En uges solrig rejse til byen Side - skyfri himmel og 38+
+			</p>
+		</li>
+	</ul>
+
+	<img src="img/filippinerne.jpg" alt="" class="photo">
+
+	<ul>
+		<li>
+			<p>
+				<strong>Juli 2011: Filippinerne</strong><br/>
+				4 ugers blandet familieferie og familiebesøg
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>November 2010: Wigan og Manchester, England</strong><br/>
+				4 dages personale ekskursion med Svendborg Ungdomsskole.<br/>
+				Besøg hos engelske skoler i forbindelse med internationalisering m.m.
+			</p>
+		</li>
+	</ul>
+
+	<img src="img/faldskaerm.jpg" alt="" class="photo">
+
+	<ul>
+		<li>
+			<p>
+				<strong>September/oktober 2010: Faldskærmsudspring</strong><br/>
+				5 spring fra 1.000 meters højde over Beldringe lufthavn.
+			</p>
+		</li>
+	</ul>
+
+	<img src="img/interrail.jpg" alt="" class="photo">
+
+	<ul>
+		<li>
+			<p>
+				<strong>Juli 2010: Interrail</strong><br/>
+				3 ugers interrail gennem flg. byer:<br/>
+				Flensborg, Tyskland - Amsterdam, Holland - Paris, Frankrig - Nice, Frankring<br/>
+				Como, Italien - Zürich, Schweiz - Wien, Østrig - Berlin, Tyskland.
+			</p>
+		</li>
+	</ul>
+
+	<h2>
+		Kontakt &amp; sociale links
+	</h2>
+	<p>
+		Jeg kan kontaktes pr. mail på: <span id="epost"><noscript>Aktiver javascript for at læse min e-mail</noscript></span>.<br/>
+		Mit CVR nr: 33 80 56 08
+	</p>
+
+	<p>
+		<strong>Sociale medier</strong>
+	</p>
+	<ul>
+		<li>
+			Twitter: <a href="http://twitter.com/henninghorn">@henninghorn</a>
+		</li>
+		<li>
+			Flickr: <a href="http://flickr.com/henninghorn">henninghorn</a>
+		</li>
+		<li>
+			Last.fm: <a href="http://last.fm/user/hhdk">hhdk</a>
+		</li>
+	</ul>
+</div>
+</body>
+</html>
